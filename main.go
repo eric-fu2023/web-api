11b270dbded49e76eb5745d79ce1a868ed3ca7d8
soethet <soethet@redfin.tech>
2023-08-29 18:13:34 +0800

    commented chat-socket

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"github.com/robfig/cron/v3"
 	"log"
 	"os"
-	"time"
 	"web-api/conf"
 	"web-api/model"
 	"web-api/server"
 	"web-api/task"
-	"web-api/task/websocket"
 )
 
 var runTask bool
@@ -34,13 +32,13 @@ func main() {
 
 	if runTask {
 		go task.ProcessFbSyncTransaction()
-		go func() {
-			for {
-				websocket.SetupWebsocket()
-				<-websocket.Websocket.Ended
-				time.Sleep(1 * time.Second)
-			}
-		}()
+		//go func() {
+		//	for {
+		//		websocket.SetupWebsocket()
+		//		<-websocket.Websocket.Ended
+		//		time.Sleep(1 * time.Second)
+		//	}
+		//}()
 
 		c := cron.New(cron.WithSeconds())
 		c.Start()
