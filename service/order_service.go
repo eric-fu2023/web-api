5677f23404c7df4f1055c71de2c37eb54a993f7e
soethet <soethet@redfin.tech>
2023-10-26 11:50:50 +0800

    fixed win amount in order history

--- service/order_service.go
+++ service/order_service.go
@@ -52,7 +52,7 @@ func (service *OrderListService) List(c *gin.Context) serializer.Response {
 	}
 
 	commonScope := model.ByOrderListConditions(user.ID, orderType[service.Type], service.IsParlay, service.IsSettled, start, end)
-	err = model.DB.Model(ploutos.BetReport{}).Scopes(commonScope).Select(`COUNT(1) as count, SUM(bet) as amount, SUM(win) as win`).Find(&orderSummary).Error
+	err = model.DB.Model(ploutos.BetReport{}).Scopes(commonScope).Select(`COUNT(1) as count, SUM(bet) as amount, SUM(win-bet) as win`).Find(&orderSummary).Error
 	if err != nil {
 		return serializer.DBErr(c, service, i18n.T("general_error"), err)
 	}
