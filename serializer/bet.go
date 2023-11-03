da12052095a238971accf7a54864cdf6af13fd64
soethet <soethet@redfin.tech>
2023-11-03 16:49:16 +0800

    added match_time in order history

--- serializer/bet.go
+++ serializer/bet.go
@@ -7,6 +7,7 @@ import (
 
 type Bet struct {
 	MatchName  string `json:"match_name"`
+	MatchTime  *int64 `json:"match_time"`
 	MarketName string `json:"market_name"`
 	OptionName string `json:"option_name"`
 	Outcome    *int64 `json:"outcome,omitempty"`
@@ -14,6 +15,7 @@ type Bet struct {
 
 func BuildBet(c *gin.Context, a ploutos.Bet) (b Bet) {
 	b.MarketName = a.GetBetType()
+	b.MatchTime = a.GetMatchTime()
 	b.OptionName = a.GetBetChoice()
 	b.MatchName = a.GetMatchName()
 	b.Outcome = a.GetOutcome()
