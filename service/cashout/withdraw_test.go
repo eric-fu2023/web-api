26c161c0413173bd41a8bb7d5c2fe77814be44d4
wy <tangweiyi@tangs-MacBook-Pro.local>
2024-02-05 18:12:51 +0800

    improve payment service err logging

--- service/cashout/withdraw_test.go
+++ service/cashout/withdraw_test.go
@@ -1,7 +1,20 @@
 package cashout_test
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
 
 func TestWithdraw(t *testing.T) {
-	
+	a := json.RawMessage(`{"a":"b"}`)
+	c := struct {
+		a json.RawMessage
+		b string
+	}{
+		a: a,
+		b: "xxx",
+	}
+	t.Log(fmt.Sprintf("%s", c))
+	t.Error(1)
 }
