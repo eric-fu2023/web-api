2dd438b7b9de6e4ebc394229eba90dacbb998b48
wy <tangweiyi@tangs-MacBook-Pro.local>
2024-02-05 11:23:18 +0800

    add logs for debugging

--- model/promotion_session.go
+++ model/promotion_session.go
@@ -8,6 +8,6 @@ import (
 )
 
 func PromotionSessionGetActive(c context.Context, promotionID int64, now time.Time) (p models.PromotionSession, err error) {
-	err = DB.WithContext(c).Where("promotion_id", promotionID).Scopes(Ongoing(now, "start_at", "end_at")).First(&p).Error
+	err = DB.Debug().WithContext(c).Where("promotion_id", promotionID).Scopes(Ongoing(now, "start_at", "end_at")).First(&p).Error
 	return
 }
