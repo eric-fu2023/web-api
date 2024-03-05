3dcb15c8c252ca0d164048ad2a9fe1c006da01db
soethet <soethet@redfin.tech>
2024-03-05 14:43:40 +0800

    added contribution in dollar jackpot draw api

--- serializer/dollar_jackpot_draw.go
+++ serializer/dollar_jackpot_draw.go
@@ -10,13 +10,14 @@ type DollarJackpotDraw struct {
 	Id            int64          `json:"id"`
 	Name          string         `json:"name"`
 	Total         *float64       `json:"total,omitempty"`
+	Contribution  *float64       `json:"contribution,omitempty"`
 	StartTimeTs   int64          `json:"start_time_ts"`
 	EndTimeTs     int64          `json:"end_time_ts"`
 	DollarJackpot *DollarJackpot `json:"dollar_jackpot,omitempty"`
 	Winner        *SimpleUser    `json:"winner,omitempty"`
 }
 
-func BuildDollarJackpotDraw(c *gin.Context, a model.DollarJackpotDraw) (b DollarJackpotDraw) {
+func BuildDollarJackpotDraw(c *gin.Context, a model.DollarJackpotDraw, contribution *int64) (b DollarJackpotDraw) {
 	b = DollarJackpotDraw{
 		Id:          a.ID,
 		Name:        a.Name,
@@ -34,6 +35,10 @@ func BuildDollarJackpotDraw(c *gin.Context, a model.DollarJackpotDraw) (b Dollar
 	if a.Winner.ID != 0 {
 		t := BuildSimpleUser(c, a.Winner)
 		b.Winner = &t
+	}
+	if contribution != nil {
+		t := util.MoneyFloat(*contribution)
+		b.Contribution = &t
 	}
 	return
 }
