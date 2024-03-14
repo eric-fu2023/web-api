92f8479333779fec101c2e586484551b2a1796a2
soethet <soethet@redfin.tech>
2024-03-14 17:14:16 +0800

    added rule image url in stream game

--- serializer/stream_game.go
+++ serializer/stream_game.go
@@ -7,18 +7,22 @@ import (
 )
 
 type StreamGame struct {
-	Id     int64   `json:"id"`
-	Name   string  `json:"game_name"`
-	MinBet float64 `json:"min_bet"`
-	MaxBet float64 `json:"max_bet"`
+	Id         int64   `json:"id"`
+	Name       string  `json:"game_name"`
+	MinBet     float64 `json:"min_bet"`
+	MaxBet     float64 `json:"max_bet"`
+	RuleImgWeb string  `json:"rule_img_web"`
+	RuleImgH5  string  `json:"rule_img_h5"`
 }
 
 func BuildStreamGame(c *gin.Context, a ploutos.StreamGame) (b StreamGame) {
 	b = StreamGame{
-		Id:     a.ID,
-		Name:   a.Name,
-		MinBet: util.MoneyFloat(a.MinBet),
-		MaxBet: util.MoneyFloat(a.MaxBet),
+		Id:         a.ID,
+		Name:       a.Name,
+		MinBet:     util.MoneyFloat(a.MinBet),
+		MaxBet:     util.MoneyFloat(a.MaxBet),
+		RuleImgWeb: Url(a.RuleImgWeb),
+		RuleImgH5:  Url(a.RuleImgH5),
 	}
 	return
 }
