14785df92e6772b5da5d3236bfe982da8884d8fe
Jia Jie <jiajie@redfin.tech>
2024-04-11 11:17:26 +0800

    Return category_id in GameVendor

--- serializer/game_vendor.go
+++ serializer/game_vendor.go
@@ -5,22 +5,24 @@ import (
 )
 
 type GameVendor struct {
-	ID      int64  `json:"id"`
-	Name    string `json:"name"`
-	GameId  int64  `json:"game_id,omitempty"`
-	WebIcon string `json:"web_icon,omitempty"`
-	AppIcon string `json:"app_icon,omitempty"`
-	Status  int64  `json:"status"`
+	ID         int64  `json:"id"`
+	Name       string `json:"name"`
+	GameId     int64  `json:"game_id,omitempty"`
+	CategoryId int64  `json:"category_id,omitempty"`
+	WebIcon    string `json:"web_icon,omitempty"`
+	AppIcon    string `json:"app_icon,omitempty"`
+	Status     int64  `json:"status"`
 }
 
 func BuildGameVendor(a ploutos.GameVendor, gameId int64) (b GameVendor) {
 	b = GameVendor{
-		ID:      a.ID,
-		Name:    a.Name,
-		GameId:  gameId,
-		WebIcon: Url(a.WebIcon),
-		AppIcon: Url(a.AppIcon),
-		Status:  a.Status,
+		ID:         a.ID,
+		Name:       a.Name,
+		GameId:     gameId,
+		CategoryId: a.CategoryId,
+		WebIcon:    Url(a.WebIcon),
+		AppIcon:    Url(a.AppIcon),
+		Status:     a.Status,
 	}
 	return
 }
