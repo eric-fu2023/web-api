27368aa6983e02a9f351c87a28a9e5ca8b43831a
soethet <soethet@redfin.tech>
2024-05-07 16:37:17 +0800

    fixed channel in app_update

--- middleware/channel.go
+++ middleware/channel.go
@@ -2,11 +2,15 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
+	"web-api/util"
 )
 
 func Channel() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		channel := c.GetHeader("Channel")
+		var channel string
+		if deviceInfo, e := util.GetDeviceInfo(c); e == nil {
+			channel = deviceInfo.Channel
+		}
 		c.Set("_channel", channel)
 		c.Next()
 	}
