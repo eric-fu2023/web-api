e9b1026c1add72c43445bf3825424d8db107377d
ad05 <ddogcatdog@gmail.com>
2024-05-08 11:40:34 +0800

    fix: revert temporary GameIntegrationNoop

--- service/common/game_integration.go
+++ service/common/game_integration.go
@@ -18,6 +18,30 @@ var GameIntegration = map[int64]GameIntegrationInterface{
 	util.IntegrationIdEvo:   evo.EVO{},
 }
 
+// to delete if unused
+type GameIntegrationNoop struct {
+}
+
+func (g GameIntegrationNoop) CreateWallet(user model.User, s string) error {
+	return nil
+}
+
+func (g GameIntegrationNoop) TransferFrom(db *gorm.DB, user model.User, s string, s2 string, i int64, extra model.Extra) error {
+	return nil
+}
+
+func (g GameIntegrationNoop) TransferTo(db *gorm.DB, user model.User, sum ploutos.UserSum, s string, s2 string, i int64, extra model.Extra) (int64, error) {
+	return 0, nil
+}
+
+func (g GameIntegrationNoop) GetGameUrl(user model.User, s string, s2 string, s3 string, i int64, extra model.Extra) (string, error) {
+	return "gameintegration/url", nil
+}
+
+func (g GameIntegrationNoop) GetGameBalance(user model.User, s string, s2 string, extra model.Extra) (int64, error) {
+	return 0, nil
+}
+
 type GameIntegrationInterface interface {
 	CreateWallet(model.User, string) error
 	TransferFrom(*gorm.DB, model.User, string, string, int64, model.Extra) error
