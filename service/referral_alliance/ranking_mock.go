5485f7182d2f838a90862265230f9e5570e854de
jh <jh@redfin.tech>
2024-05-24 11:16:23 +0800

    add en names for mock referral alliance ranking data

--- service/referral_alliance/ranking_mock.go
+++ service/referral_alliance/ranking_mock.go
@@ -114,7 +114,8 @@ const (
 )
 
 type MockRanking struct {
-	Nickname             string
+	NicknameZh           string
+	NicknameEn           string
 	Avatar               string
 	DefaultReferralCount int64
 	DefaultRewardAmount  int64
@@ -123,91 +124,104 @@ type MockRanking struct {
 var (
 	mockRewardRankings = []MockRanking{
 		{
-			Nickname:             "国米·布雷特",
+			NicknameZh:           "国米·布雷特",
+			NicknameEn:           "Brendan Gilmore",
 			Avatar:               "/img/user/900/3e5408cf8ba2344e778c2e61035ccd_180_135.jpg",
 			DefaultRewardAmount:  1309509,
 			DefaultReferralCount: 135,
 		},
 		{
 
-			Nickname:             "巴萨·诺尔",
+			NicknameZh:           "巴萨·诺尔",
+			NicknameEn:           "Aedan Alvarado",
 			Avatar:               "/img/user/900/57c674ede3ecc1224beff71f6d6e5d_180_135.jpg",
 			DefaultRewardAmount:  986578,
 			DefaultReferralCount: 151,
 		},
 		{
 
-			Nickname:             "斯巴达·贝吉塔",
+			NicknameZh:           "斯巴达·贝吉塔",
+			NicknameEn:           "Louis",
 			Avatar:               "/img/user/900/6631799bb681f8cf7f6ba9dda29547_180_135.jpg",
 			DefaultRewardAmount:  503489,
 			DefaultReferralCount: 98,
 		},
 		{
 
-			Nickname:             "思考·仙女",
+			NicknameZh:           "思考·仙女",
+			NicknameEn:           "Alan",
 			Avatar:               "/img/user/900/694086c31abe683d5f7efdca7d1ba3_180_135.jpg",
 			DefaultRewardAmount:  400380,
 			DefaultReferralCount: 82,
 		},
 		{
 
-			Nickname:             "煮饭·公牛",
+			NicknameZh:           "煮饭·公牛",
+			NicknameEn:           "WarrenChauncey",
 			Avatar:               "/img/user/900/70afaf2a47802916ccc7cc9a5ff145_180_135.jpg",
 			DefaultRewardAmount:  98823,
 			DefaultReferralCount: 75,
 		},
 		{
 
-			Nickname:             "敲木鱼·曹操",
+			NicknameZh:           "敲木鱼·曹操",
+			NicknameEn:           "JanineMalissa",
 			Avatar:               "/img/user/900/959267dd8549f78d3d3b4eeeeebcf2_180_135.jpg",
 			DefaultRewardAmount:  45609,
 			DefaultReferralCount: 34,
 		},
 		{
 
-			Nickname:             "躲雨·荔枝",
+			NicknameZh:           "躲雨·荔枝",
+			NicknameEn:           "AllysonRenata",
 			Avatar:               "/img/user/900/98949ee9b97c21787f3fcdf0ffbbd5_180_135.jpg",
 			DefaultRewardAmount:  35515,
 			DefaultReferralCount: 28,
 		},
 		{
 
-			Nickname:             "坐电梯·仙女神",
+			NicknameZh:           "坐电梯·仙女神",
+			NicknameEn:           "TrinaOctavia",
 			Avatar:               "/img/user/900/aca55cb864f180994847f157bf8354_180_135.jpg",
 			DefaultRewardAmount:  15023,
 			DefaultReferralCount: 25,
 		},
 		{
 
-			Nickname:             "喝豆奶·编剧",
+			NicknameZh:           "喝豆奶·编剧",
+			NicknameEn:           "MarcieShonda",
 			Avatar:               "/img/user/900/afb5c20ba9ba21679108296b3462d8_180_135.jpg",
 			DefaultRewardAmount:  6323,
 			DefaultReferralCount: 12,
 		},
 		{
 
-			Nickname:             "迫降·贝斯",
+			NicknameZh:           "迫降·贝斯",
+			NicknameEn:           "KenyaKathrine",
 			Avatar:               "/img/user/900/b65a36cbfe158ab6cd71077fd62892_180_135.jpg",
 			DefaultRewardAmount:  2503,
 			DefaultReferralCount: 3,
 		},
 		{
 
-			Nickname:             "富勒姆·布雷",
+			NicknameZh:           "富勒姆·布雷",
+			NicknameEn:           "RavenRosanna",
 			Avatar:               "/img/user/900/c7b8179417ecadeab55e9036fb99e1_180_135.jpg",
 			DefaultRewardAmount:  0,
 			DefaultReferralCount: 122,
 		},
 		{
 
-			Nickname:             "奥林匹亚科斯·威少",
+			NicknameZh:           "奥林匹亚科斯·威少",
+			NicknameEn:           "CasandraAda",
 			Avatar:               "/img/user/900/d949607c3cb2dd3148ed24bbfaefcf_180_135.jpg",
 			DefaultRewardAmount:  0,
 			DefaultReferralCount: 101,
 		},
 		{
 
-			Nickname:             "掘金·希尔",
+			NicknameZh:           "掘金·希尔",
+			NicknameEn:           "TomPete",
 			Avatar:               "/img/user/900/e360dd9a5548b9205bd7374eeb0166_180_135.jpg",
 			DefaultRewardAmount:  0,
 			DefaultReferralCount: 87,
