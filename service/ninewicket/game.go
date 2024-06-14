5b19ca9304f90af7bac3c1bffd959a6446c9efe1
yongyao.lee <yongyao.lee@up-devlabs.io>
2024-06-14 11:07:09 +0800

    add 9 wicket event type param

--- service/ninewicket/game.go
+++ service/ninewicket/game.go
@@ -18,13 +18,16 @@ func (n *NineWicket) GetGameUrl(user model.User, currency, gameCode, subGameCode
 	//currentTimeMillis := time.Now().UnixNano() / int64(time.Millisecond)
 	//currentTimeMillisString := strconv.FormatInt(currentTimeMillis, 10)
 
-	key, err := client.PlayerGetKey(user.IdAsString(), user.Username, "testbainrma", api.CurrencyINR)
-
-	url, err = client.PlayerGetLoginUrl(user.IdAsString(), &key, 4)
+	key, err := client.PlayerGetKey(user.IdAsString(), user.Username, "testbainrma", api.EventTypeCricket)
+	if err != nil {
+		log.Printf("Error getting 9Wicket key, err: %v ", err.Error())
+	}
+	url, err = client.PlayerGetLoginUrl(user.IdAsString(), &key, api.EventTypeCricket)
 
 	if err != nil {
 		log.Printf("Error getting 9Wicket game url, err: %v ", err.Error())
 	}
+
 	//url = os.Getenv("GAME_EVO_HOST") + url.EntryEmebedded
 
 	return url, err
