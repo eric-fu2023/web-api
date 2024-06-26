495ec4c3ee76d7e22b1069af0762b2d79bc243f2
juicy <juicy@redfin.tech>
2024-06-26 17:00:54 +0800

    Get title and smallicon

--- serializer/game_vendor_brand.go
+++ serializer/game_vendor_brand.go
@@ -9,10 +9,12 @@ import (
 type GameVendorBrand struct {
 	ID               int64  `json:"id"`
 	Name             string `json:"name"`
+	Title            string `json:"title"`
 	GameId           int64  `json:"game_id,omitempty"`
 	Type             int64  `json:"type"`
 	WebIcon          string `json:"web_icon,omitempty"`
 	AppIcon          string `json:"app_icon,omitempty"`
+	SmallIcon        string `json:"small_icon"`
 	IsMaintenance    bool   `json:"is_maintenance,omitempty"`
 	MaintenanceStart int64  `json:"maintenance_start,omitempty"`
 	MaintenanceEnd   int64  `json:"maintenance_end,omitempty"`
@@ -20,11 +22,13 @@ type GameVendorBrand struct {
 
 func BuildGameVendorBrand(a ploutos.GameVendorBrand) (b GameVendorBrand) {
 	b = GameVendorBrand{
-		ID:      a.GameVendorId,
-		Name:    a.Name,
-		Type:    a.CategoryId,
-		WebIcon: Url(a.WebIcon),
-		AppIcon: Url(a.AppIcon),
+		ID:        a.GameVendorId,
+		Name:      a.Name,
+		Title:     a.Title,
+		Type:      a.CategoryId,
+		WebIcon:   Url(a.WebIcon),
+		AppIcon:   Url(a.AppIcon),
+		SmallIcon: Url(a.SmallIcon),
 	}
 	if !a.StartTime.IsZero() {
 		if time.Now().After(a.StartTime) && (time.Now().Before(a.EndTime) || a.EndTime.IsZero()) {
@@ -41,12 +45,14 @@ func BuildGameVendorBrand(a ploutos.GameVendorBrand) (b GameVendorBrand) {
 // only used for GameCategoryListService
 func BuildGameVendorBrandForCategory(a ploutos.GameVendorBrand, gameId int64) (b GameVendorBrand) {
 	b = GameVendorBrand{
-		ID:      a.GameVendorId,
-		Name:    a.Name,
-		GameId:  gameId,
-		Type:    a.CategoryId,
-		WebIcon: Url(a.WebIcon),
-		AppIcon: Url(a.AppIcon),
+		ID:        a.GameVendorId,
+		Name:      a.Name,
+		Title:     a.Title,
+		GameId:    gameId,
+		Type:      a.CategoryId,
+		WebIcon:   Url(a.WebIcon),
+		AppIcon:   Url(a.AppIcon),
+		SmallIcon: Url(a.SmallIcon),
 	}
 	if !a.StartTime.IsZero() {
 		if time.Now().After(a.StartTime) && (time.Now().Before(a.EndTime) || a.EndTime.IsZero()) {
