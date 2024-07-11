05a105f97fee05a26f88fd29c6ed94cc1c4d2d6c
juicy <juicy@redfin.tech>
2024-07-11 10:55:39 +0800

    Removed " x " from roomMessage

--- service/common/common.go
+++ service/common/common.go
@@ -420,7 +420,7 @@ func SendGiftSocketMessage(userId int64, giftId int64, giftQuantity int, giftNam
 				default:
 					msg := websocket.RoomMessage{
 						Room:           fmt.Sprintf(`stream:%d`, liveStreamId),
-						Message:        message + giftName + " " + strconv.Itoa(giftQuantity) + " x ",
+						Message:        message + giftName + " " + strconv.Itoa(giftQuantity),
 						UserId:         userId,
 						UserType:       consts.ChatUserType["user"],
 						Nickname:       nickname,
