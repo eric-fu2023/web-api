2834d5065d93d4d857237cc8e962f40055604753
Seng Wei Han <seng@redfin.tech>
2024-07-15 21:14:53 +0800

    feat: add implementation for create wallet for mumbai game

--- service/mumbai/account.go
+++ service/mumbai/account.go
@@ -3,16 +3,41 @@ package mumbai
 import (
 	"fmt"
 	"web-api/model"
+	"web-api/util"
+
+	ploutos "blgit.rfdev.tech/taya/ploutos-object"
+	"gorm.io/gorm"
 )
 
 func (c *Mumbai) CreateWallet(user model.User, currency string) error {
-	return c.createMumbaiUserAndDbWallet(user, currency)
-}
+	// create a record for the user for mumbai game.
 
-// skips duplicate error on insert.
-func (c *Mumbai) createMumbaiUserAndDbWallet(user model.User, currency string) error {
+	return model.DB.Transaction(func(tx *gorm.DB) (err error) {
+		var gameVendors []ploutos.GameVendor
+		err = tx.Model(ploutos.GameVendor{}).Joins(`INNER JOIN game_vendor_brand gvb ON gvb.game_vendor_id = game_vendor.id`).
+			Where(`game_vendor.game_integration_id`, util.IntegrationIdMumbai).Find(&gameVendors).Error
 
-	return fmt.Errorf("not implemented") // @Seng
+		if err != nil {
+			return
+		}
+
+		externalUserId := c.Merchant + c.Agent + user.IdAsString()
+
+		for _, gameVendor := range gameVendors {
+			gvu := ploutos.GameVendorUser{
+				GameVendorId:     gameVendor.ID,
+				UserId:           user.ID,
+				ExternalUserId:   externalUserId, // USE THE USERNAME WITH SPECIFIC RULE
+				ExternalCurrency: currency,
+			}
+
+			err = tx.Create(&gvu).Error
+			if err != nil {
+				return
+			}
+		}
+		return
+	})
 
 }
 
