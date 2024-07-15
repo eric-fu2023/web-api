5691d4a6ad6d2123fe2ce57d9de821ca360b4f28
Seng Wei Han <seng@redfin.tech>
2024-07-16 01:19:50 +0800

    feat: add initial implementation for transfer_from function

--- service/mumbai/types.go
+++ service/mumbai/types.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 )
 
+var ErrInsufficientMumbaiWalletBalance = errors.New("insufficient mumbai wallet balance")
 var ErrGetBalance = errors.New("Mumbai get balance error")
 var ErrGameCodeMapping = errors.New("game code mapping error")
 
@@ -11,4 +12,5 @@ type ResponseCode string
 
 const (
 	ResponseCodeNotAccountFoundError ResponseCode = "EX002"
+	ResponseCodeNotEnoughFundsError  ResponseCode = "EX007"
 )
--- service/mumbai/wallet.go
+++ service/mumbai/wallet.go
@@ -2,19 +2,76 @@ package mumbai
 
 import (
 	"fmt"
+	"strconv"
 	"web-api/model"
 	"web-api/util"
 
 	ploutos "blgit.rfdev.tech/taya/ploutos-object"
-
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
-// TransferFrom
+// CURRENTLY USING HARDCODED VALUE. NOTE: WILL NEED TO CHANGE TO USE USER INPUT.
+const (
+	defaultTransactionNum = "12345678"
+	defaultTransferAmount = "50"
+)
+
 func (c *Mumbai) TransferFrom(tx *gorm.DB, user model.User, currency, gameCode string, gameVendorId int64, extra model.Extra) error {
-	client, _ := util.MumbaiFactory()
-	_ = client
-	return fmt.Errorf("not implemented")
+	// get the balance from mumbai and update the db
+	client, err := util.MumbaiFactory()
+	if err != nil {
+		return err
+	}
+
+	username := c.Merchant + c.Agent + user.IdAsString()
+	transactionNo := c.Merchant + defaultTransactionNum
+
+	res, err := client.WithdrawUser(username, transactionNo, defaultTransferAmount)
+
+	if err != nil {
+		if err.Error() == string(ResponseCodeNotEnoughFundsError) {
+			return ErrInsufficientMumbaiWalletBalance
+		} else {
+			return err
+		}
+	}
+
+	// if no error means we have sufficient funds to withdraw from mumbai server
+	// now we will need to update the balance in db to match with the withdraw amount.
+	// parse the money from string -> float64 -> int64
+	money, err := strconv.ParseFloat(res.Result.Money, 64)
+	moneyToInsertDB := util.MoneyInt(money)
+
+	if err != nil {
+		return err
+	}
+
+	var sum ploutos.UserSum
+	if err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Where(`user_id`, user.ID).First(&sum).Error; err != nil {
+		return err
+	}
+
+	transaction := ploutos.Transaction{
+		UserId:                user.ID,
+		Amount:                moneyToInsertDB,
+		BalanceBefore:         sum.Balance,
+		BalanceAfter:          sum.Balance + moneyToInsertDB,
+		TransactionType:       ploutos.TransactionTypeFromGameIntegration,
+		Wager:                 0,
+		WagerBefore:           sum.RemainingWager,
+		WagerAfter:            sum.RemainingWager,
+		ExternalTransactionId: "", // empty string for now
+		GameVendorId:          gameVendorId,
+	}
+
+	err = tx.Create(&transaction).Error
+	if err != nil {
+		return err
+	}
+
+	err = tx.Model(ploutos.UserSum{}).Where(`user_id`, user.ID).Update(`balance`, gorm.Expr(`balance + ?`, moneyToInsertDB)).Error
+	return err
 }
 
 func (c *Mumbai) TransferTo(tx *gorm.DB, user model.User, sum ploutos.UserSum, _currency, gameCode string, gameVendorId int64, extra model.Extra) (_transferredBalance int64, _err error) {
