469c0942c34faf7c27e89c4cc2c122dbca27bcfd
noel <noel@redfin.tech>
2024-07-15 10:59:43 +0800

    scaffold

--- service/mumbai/account.go
+++ service/mumbai/account.go
@@ -2,16 +2,20 @@ package imone
 
 import (
 	"fmt"
+	"web-api/util"
 
 	"web-api/model"
 )
 
 func (c *Mumbai) CreateWallet(user model.User, currency string) error {
-	return c.createImOneUserAndDbWallet(user, currency)
+	return c.createMumbaiUserAndDbWallet(user, currency)
 }
 
 // skips duplicate error on insert.
-func (c *Mumbai) createImOneUserAndDbWallet(user model.User, currency string) error {
+func (c *Mumbai) createMumbaiUserAndDbWallet(user model.User, currency string) error {
+	client, _ := util.MumbaiFactory()
+	_ = client
+	// call game service :client.LoginUser()
 	return fmt.Errorf("not implemented") // @Seng
 
 }
--- service/mumbai/wallet.go
+++ service/mumbai/wallet.go
@@ -4,12 +4,15 @@ import (
 	ploutos "blgit.rfdev.tech/taya/ploutos-object"
 	"fmt"
 	"web-api/model"
+	"web-api/util"
 
 	"gorm.io/gorm"
 )
 
 // TransferFrom
 func (c *Mumbai) TransferFrom(tx *gorm.DB, user model.User, currency, gameCode string, gameVendorId int64, extra model.Extra) error {
+	client, _ := util.MumbaiFactory()
+	client.CheckBalanceUser("someuser")
 	return fmt.Errorf("not implemented")
 }
 
