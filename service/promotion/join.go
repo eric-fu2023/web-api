f18a0dcf76bde10f11956e7d5ab0a162008d3b74
juicy <juicy@redfin.tech>
2024-07-24 17:49:51 +0800

    Done join promotion

--- service/promotion/join.go
+++ service/promotion/join.go
@@ -3,11 +3,13 @@ package promotion
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
 	"web-api/model"
 	"web-api/serializer"
 	"web-api/util/i18n"
 
+	models "blgit.rfdev.tech/taya/ploutos-object"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,7 +25,7 @@ type PromotionJoinError struct {
 func (p PromotionJoin) Handle(c *gin.Context) (r serializer.Response, err error) {
 	now := time.Now().UTC()
 	brand := c.MustGet(`_brand`).(int)
-	// user := c.MustGet("user").(model.User)
+	user := c.MustGet("user").(model.User)
 	// deviceInfo, _ := util.GetDeviceInfo(c)
 	i18n := c.MustGet("i18n").(i18n.I18n)
 
@@ -48,46 +50,55 @@ func (p PromotionJoin) Handle(c *gin.Context) (r serializer.Response, err error)
 	}
 
 	isExceeded := false
+	data := make(map[string]string)
+	for _, field := range incomingRequestAction.Fields {
+		switch field.Type {
+		case "input-button":
+			isExceeded, err = serializer.ParseButtonClickOption(c, field, p.PromotionId, user.ID)
+			if err != nil {
+				r = serializer.Err(c, p, serializer.CodeGeneralError, i18n.T("custom_promotion_entry_fail"), err)
+				return
+			}
+		case "input-dropdown":
+			if requestInput[strconv.Itoa(field.InputId)] != "" {
+				index, _ := strconv.Atoi(requestInput[strconv.Itoa(field.InputId)])
+				index--
 
-	// for _, field := range incomingRequestAction.Fields {
-	// 	switch field.Type {
-	// 	case "input-button":
-	// 		isExceeded, err = serializer.ParseButtonClickOption(c, field, p.PromotionId, user.ID)
-	// 		if err != nil {
-	// 			r = serializer.Err(c, p, serializer.CodeGeneralError, i18n.T("custom_promotion_entry_fail"), err)
-	// 			return
-	// 		}
-	// 	case "input-dropdown":
-	// 		if requestInput[strconv.Itoa(field.InputId)] != "" {
-	// 			if fieldType {
-
-	// 			}
-	// 		}
-	// 	case "input-keyin":
-
-	// 	}
-	// }
+				if index >= len(field.Options)-1 {
+					continue
+				}
+				option := field.Options[index]
+				for _, value := range option {
+					data[field.Title] = value
+				}
+			}
+		case "input-keyin":
+			if requestInput[strconv.Itoa(field.InputId)] != "" {
+				data[field.Title] = requestInput[strconv.Itoa(field.InputId)]
+			}
+		}
+	}
 
 	if isExceeded {
 		r = serializer.Err(c, p, serializer.CodeGeneralError, i18n.T("custom_promotion_entry_exceed"), err)
 		return
 	}
 
-	// jsonInput, _ := json.Marshal(requestInput)
+	jsonInput, _ := json.Marshal(data)
 
-	// request := models.PromotionRequest{
-	// 	PromotionId:  p.PromotionId,
-	// 	UserId:       user.ID,
-	// 	Status:       1, // Pending
-	// 	InputDetails: jsonInput,
-	// }
+	request := models.PromotionRequest{
+		PromotionId:  p.PromotionId,
+		UserId:       user.ID,
+		Status:       1, // Pending
+		InputDetails: jsonInput,
+	}
 
-	// err = model.CreateJoinCustomPromotion(request)
+	err = model.CreateJoinCustomPromotion(request)
 
-	// if err != nil {
-	// 	r = serializer.Err(c, p, serializer.CodeGeneralError, i18n.T("custom_promotion_entry_fail"), err)
-	// 	return
-	// }
+	if err != nil {
+		r = serializer.Err(c, p, serializer.CodeGeneralError, i18n.T("custom_promotion_entry_fail"), err)
+		return
+	}
 
 	if len(errorFields) > 0 {
 		r = serializer.Response{
