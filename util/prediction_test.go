3ad0375130a6e8e5ab31cd2a5e773096c6229e89
carl <carl@redfin.tech>
2024-08-06 17:13:18 +0800

    add util for nearXwinX

---
+++ util/prediction_test.go
@@ -0,0 +1,60 @@
+package util_test
+
+import (
+	"fmt"
+	"testing"
+	"web-api/util"
+)
+
+type consecutiveWinCase struct {
+	data     []bool
+	expected int
+}
+
+var consecutiveWinCases = []consecutiveWinCase{
+	// {[]bool{true, true, true, true, true, false, true, true, true, false}, 5},
+	// {[]bool{true, true, false, true, true, true, true, true, true, true, false}, 7},
+	// {[]bool{true, true, false, false, true, true, true, true, true}, 0},
+}
+
+func TestConsecutiveWins(t *testing.T) {
+	fmt.Println("=======TestConsecutiveWins=======")
+	for i, test := range consecutiveWinCases {
+		fmt.Printf("Testing case %v : \n", test.data)
+		if output := util.ConsecutiveWins(test.data); output != test.expected {
+			fmt.Printf("ERROR!! Expected %d, got %d\n", test.expected, output)
+			t.Errorf("Test %d - Output %d not equal to expected %d", i, output, test.expected)
+		} else {
+			fmt.Printf("OK\n")
+		}
+	}
+}
+
+type nearXWinXCase struct {
+	data         []bool
+	expectedNear int
+	expectedWin  int
+}
+
+var nearXWinXCases = []nearXWinXCase{
+	{[]bool{true, true, true, false, true, true, false, true, true, false, false}, 11, 7},
+	{[]bool{true, false, true, true, true, false, true, true, false, false, true, false}, 10, 6},
+	{[]bool{true, false, true, true, true, true, true, true, true, true, false, true}, 10, 9},
+	{[]bool{true, true, true, true, true, true, true, true, true, true, true, true}, 12, 12},
+	{[]bool{false, false, false, false, false, false, false, false, false, false, true, true}, 6, 2},
+	{[]bool{true, true, false, false, false, true, false, true, false, false, true, true}, 7, 4},
+}
+
+func TestNearXWinX(t *testing.T) {
+	fmt.Println("=======TestNearXWinX=======")
+
+	for i, test := range nearXWinXCases {
+		fmt.Printf("Testing case %v : \n", test.data)
+		if outNear, outWin := util.NearXWinX(test.data); outNear != test.expectedNear || outWin != test.expectedWin {
+			fmt.Printf("ERROR!! Expected (%d,%d), got (%d,%d)\n", test.expectedNear, test.expectedWin, outNear, outWin)
+			t.Errorf("Test %d - Output (%d,%d) not equal to expected (%d,%d)", i, outNear, outWin, test.expectedNear, test.expectedWin)
+		} else {
+			fmt.Printf("OK\n")
+		}
+	}
+}
