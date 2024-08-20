3b62715dd1fc967d284a2dca9320e918f8a60110
Du Wei <perfuma@redfin.tech>
2024-08-20 16:00:06 +0800

    if no ongoing djd, take the latest completed djd

--- service/dollar_jackpot/list.go
+++ service/dollar_jackpot/list.go
@@ -48,20 +48,19 @@ func (service *DollarJackpotGetService) Get(c *gin.Context) (r serializer.Respon
 		}
 	}
 	var data *serializer.DollarJackpotDraw
-	// if dollarJackpotDraw.ID == 0 { // if there is no ongoing draw
-	// 	var djd model.DollarJackpotDraw
-	// 	err = model.DB.WithContext(ctx).Joins(`JOIN dollar_jackpots ON dollar_jackpots.status = 1 AND dollar_jackpots.id = dollar_jackpot_draws.dollar_jackpot_id AND dollar_jackpots.brand_id = ?`, brand).
-	// 		Where(`dollar_jackpot_draws.status != ?`, 0).Order(`start_time DESC`).
-	// 		Preload(`DollarJackpot`).Limit(1).Find(&djd).Error
-	// 	if err != nil {
-	// 		r = serializer.Err(c, service, serializer.CodeGeneralError, i18n.T("general_error"), err)
-	// 		return
-	// 	}
-	// 	if djd.ID != 0 {
-	// 		dollarJackpotDraw = djd
-	// 		dollarJackpotDraw.Total = &dollarJackpotDraw.DollarJackpot.Prize
-	// 	}
-	// }
+	if dollarJackpotDraw.ID == 0 { // if there is no ongoing draw
+		var djd model.DollarJackpotDraw
+		err = model.DB.WithContext(ctx).Joins(`JOIN dollar_jackpots ON dollar_jackpots.status = 1 AND dollar_jackpots.id = dollar_jackpot_draws.dollar_jackpot_id AND dollar_jackpots.brand_id = ?`, brand).
+			Where(`dollar_jackpot_draws.status != 0`).Where(`dollar_jackpots.streamer_id`, service.StreamerId).Order(`start_time DESC`).Preload(`DollarJackpot`).Limit(1).Find(&djd).Error
+		if err != nil {
+			r = serializer.Err(c, service, serializer.CodeGeneralError, i18n.T("general_error"), err)
+			return
+		}
+		if djd.ID != 0 {
+			dollarJackpotDraw = djd
+			dollarJackpotDraw.Total = &dollarJackpotDraw.DollarJackpot.Prize
+		}
+	}
 	data, err = prepareObj(c, dollarJackpotDraw)
 	if err != nil {
 		r = serializer.Err(c, service, serializer.CodeGeneralError, i18n.T("general_error"), err)
