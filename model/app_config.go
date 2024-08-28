467db56f600aaf3e72aaafebeabd855b36a1bf25
juicy <juicy@redfin.tech>
2024-08-28 15:43:39 +0800

    Updated cache expiry

--- model/app_config.go
+++ model/app_config.go
@@ -1,10 +1,23 @@
 package model
 
+import (
+	"sync"
+	"time"
+)
+
 var (
-	appConfigCache = map[string]map[string]string{}
+	appConfigCache            = map[string]map[string]string{}
+	appConfigCacheLastUpdate  int64
+	appConfigCacheTimeMinutes = 10
+	cacheMutex                sync.Mutex
 )
 
 func GetAppConfigWithCache(name, key string) (string, error) {
+	checkIfNeedToExpireCache()
+
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+
 	if v, ok := appConfigCache[name]; ok {
 		if vv, ok := v[key]; ok {
 			return vv, nil
@@ -31,4 +44,20 @@ func GetAppConfig(name, key string) (string, error) {
 		return "", err
 	}
 	return value, nil
+}
+
+func checkIfNeedToExpireCache() {
+
+	cacheLifeSpanSeconds := int64(appConfigCacheTimeMinutes * 60)
+
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
+
+	if appConfigCacheLastUpdate == 0 || time.Now().UTC().Unix()-appConfigCacheLastUpdate > cacheLifeSpanSeconds {
+		// Cache expired
+		appConfigCacheLastUpdate = time.Now().UTC().Unix()
+
+		// Empty cache
+		appConfigCache = map[string]map[string]string{}
+	}
 }
