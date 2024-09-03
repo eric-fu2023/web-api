b6539c2e3a0d7aecd506394cc54045aa3888715e
Du Wei <perfuma@redfin.tech>
2024-09-03 12:47:17 +0800

    add settlement time ts

--- serializer/dollar_jackpot_draw.go
+++ serializer/dollar_jackpot_draw.go
@@ -16,18 +16,28 @@ type DollarJackpotDraw struct {
 	ContributionLimit *float64       `json:"contribution_limit,omitempty"`
 	StartTimeTs       int64          `json:"start_time_ts"`
 	EndTimeTs         int64          `json:"end_time_ts"`
+	SettleTimeTs      int64          `json:"settle_time_ts"`
 	IsClosed          bool           `json:"is_closed"`
 	DollarJackpot     *DollarJackpot `json:"dollar_jackpot,omitempty"`
 	Winner            *SimpleUser    `json:"winner,omitempty"`
 }
 
 func BuildDollarJackpotDraw(c *gin.Context, a model.DollarJackpotDraw, contribution *int64) (b DollarJackpotDraw) {
+	endUnix := a.EndTime.Unix()
+
+	// Calculate the next 0 or 5 seconds interval
+	nextInterval := endUnix + (5-(endUnix%5))%5
+
+	// Add 13 seconds to get SettleTimeTs
+	settleTimeTs := nextInterval + 13
+
 	b = DollarJackpotDraw{
-		Id:          a.ID,
-		Name:        a.Name,
-		StartTimeTs: a.StartTime.Unix(),
-		EndTimeTs:   a.EndTime.Unix(),
-		Status:      a.DollarJackpotDraw.Status,
+		Id:           a.ID,
+		Name:         a.Name,
+		StartTimeTs:  a.StartTime.Unix(),
+		EndTimeTs:    a.EndTime.Unix(),
+		SettleTimeTs: settleTimeTs,
+		Status:       a.DollarJackpotDraw.Status,
 	}
 	if a.Total != nil {
 		t := util.MoneyFloat(*a.Total)
