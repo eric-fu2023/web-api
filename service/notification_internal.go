87c66219cdbd589c4212edc0516aa0f0f07fd5df
Seng Wei Han <seng@redfin.tech>
2024-09-03 15:20:21 +0800

    add title and desc for win/lose push notification

--- service/notification_internal.go
+++ service/notification_internal.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
 	"web-api/conf"
 	"web-api/conf/consts"
 	"web-api/model"
@@ -15,6 +17,8 @@ const (
 	vipPromoteNote = "vip_promotion"
 	popUpNote      = "popup_winlose"
 	spinNote       = "spin"
+	minIndex       = 0
+	maxIndex       = 2
 )
 
 type InternalNotificationPushRequest struct {
@@ -26,6 +30,7 @@ type InternalNotificationPushRequest struct {
 func (p InternalNotificationPushRequest) Handle(c *gin.Context) (r serializer.Response) {
 	var notificationType, title, text string
 	lang := model.GetUserLang(p.UserID)
+
 	switch p.Type {
 	case vipPromoteNote:
 		notificationType = consts.Notification_Type_Vip_Promotion
@@ -35,10 +40,33 @@ func (p InternalNotificationPushRequest) Handle(c *gin.Context) (r serializer.Re
 			vipName = p.Params["vip_level"]
 		}
 		text = fmt.Sprintf(conf.GetI18N(lang).T(common.NOTIFICATION_VIP_PROMOTION), vipName)
+
 	case popUpNote:
 		notificationType = consts.Notification_Type_Pop_Up
-		title = conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WINLOSE_TITLE)
-		text = conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WINLOSE)
+		popUpTitle := []string{conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WINLOSE_FIRST_TITLE), conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WINLOSE_SECOND_TITLE), conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WINLOSE_THIRD_TITLE)}
+		popUpWinDesc := []string{conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WIN_FIRST_DESC), conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WIN_SECOND_DESC), conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_WIN_THIRD_DESC)}
+		popUpLoseDesc := []string{conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_LOSE_FIRST_DESC), conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_LOSE_SECOND_DESC), conf.GetI18N(lang).T(common.NOTIFICATION_POPUP_LOSE_THIRD_DESC)}
+
+		win_lose, _ := strconv.ParseInt(p.Params["win_lose_amount"], 10, 64)
+		rank, _ := strconv.ParseInt(p.Params["ranking"], 10, 64)
+
+		// randomly pick titles between the 3
+		// get the win/lose value from params and determine whether to display win/lose desc
+		randIndex := rand.Intn(maxIndex - minIndex + 1)
+
+		if win_lose < 0 {
+			// meaning the user is currently losing money.
+			lose := win_lose * -1 // negate
+			ranking := rank * -1
+
+			text = fmt.Sprintf(popUpLoseDesc[randIndex], lose, ranking)
+
+		} else {
+			text = fmt.Sprintf(popUpWinDesc[randIndex], win_lose, rank)
+		}
+
+		title = popUpTitle[randIndex]
+
 	case spinNote:
 		notificationType = consts.Notification_Type_Spin
 		title = conf.GetI18N(lang).T(common.NOTIFICATION_SPIN_TITLE)
