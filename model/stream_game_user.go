9e79f9c26530bb7584eafc7816a8653d0f19f63e
juicy <juicy@redfin.tech>
2024-09-10 11:35:26 +0800

    Added link for stream game streamer

---
+++ model/stream_game_user.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	ploutos "blgit.rfdev.tech/taya/ploutos-object"
+	"gorm.io/gorm"
+)
+
+const ()
+
+func GetStreamGameStreamer(userId, gameId int64) (hasGame bool, err error) {
+	var record ploutos.StreamGameUser
+	err = DB.Transaction(func(tx *gorm.DB) error {
+		tx = tx.Table("stream_game_users").
+			Where("user_id = ?", userId).
+			Where("stream_game_id = ?", gameId)
+		if err := tx.First(&record).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if record.ID == 0 || !record.IsActive {
+		return
+	}
+
+	hasGame = true
+	return
+}
+
+func ToggleStreamerStreamGame(streamerId, gameId int64, status bool) (err error) {
+
+	var game ploutos.StreamGameUser
+
+	_ = DB.Table("stream_game_users").
+		Where("user_id = ?", streamerId).
+		Where("stream_game_id = ?", gameId).
+		First(&game).Error
+
+	if game.ID == 0 {
+		game.UserId = streamerId
+		game.StreamGameId = gameId
+	}
+
+	game.IsActive = status
+	err = DB.Save(&game).Error
+
+	return
+}
