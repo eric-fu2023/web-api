83a06f50208ed1c521a8c75425b922471bfcaf6f
noel <noel@redfin.tech>
2024-09-16 09:05:32 +0800

    rename var

--- service/cashin/promotion_handler.go
+++ service/cashin/promotion_handler.go
@@ -15,10 +15,10 @@ func HandlePromotion(c context.Context, order model.CashOrder) {
 	service.HandleCashMethodPromotion(c, order)
 }
 
-func HandleOneTimeBonus(c context.Context, order model.CashOrder) {
+func HandleOneTimeBonus(ctx context.Context, order model.CashOrder) {
 	var user model.User
 	if err := model.DB.Where(`id`, order.UserId).First(&user).Error; err != nil {
-		util.GetLoggerEntry(c).Error("get user error", err)
+		util.GetLoggerEntry(ctx).Error("get user error", err)
 		return
 	}
 	// uaCond := model.GetUserAchievementCond{AchievementIds: []int64{
@@ -38,35 +38,38 @@ func HandleOneTimeBonus(c context.Context, order model.CashOrder) {
 	// }
 	amt, err := service.GetCachedConfigBranded(context.Background(), "static_promotion_one_time_bonus_min_amount", user.BrandId)
 	if err != nil {
-		util.GetLoggerEntry(c).Error("get config error", err)
+		util.GetLoggerEntry(ctx).Error("get config error", err)
 		return
 	}
 	minAmt, _ := strconv.Atoi(amt)
 	if order.AppliedCashInAmount < int64(minAmt) {
-		util.GetLoggerEntry(c).Info("insufficient amount", order.AppliedCashInAmount)
+		util.GetLoggerEntry(ctx).Info("insufficient amount", order.AppliedCashInAmount)
 		return
 	}
-	v, err := service.GetCachedConfigBranded(context.Background(), "static_promotion_one_time_bonus_id", user.BrandId)
+	cachedPromotionId, err := service.GetCachedConfigBranded(context.Background(), "static_promotion_one_time_bonus_id", user.BrandId)
 	if err != nil {
-		util.GetLoggerEntry(c).Error("get config error", err)
+		util.GetLoggerEntry(ctx).Error("get config error", err)
 		return
 	}
-	id, _ := strconv.Atoi(v)
-
+	promotionId, err := strconv.ParseInt(cachedPromotionId, 10, 64)
+	if err != nil {
+		util.GetLoggerEntry(ctx).Error("conv cachedPromotionId error", err)
+		return
+	}
 	now := time.Now()
-	p, err := model.PromotionGetActive(context.TODO(), int(user.BrandId), int64(id), now)
+	p, err := model.PromotionGetActive(context.TODO(), int(user.BrandId), int64(promotionId), now)
 	if err != nil {
-		util.GetLoggerEntry(c).Error("get promotion error", err)
+		util.GetLoggerEntry(ctx).Error("get promotion error", err)
 		return
 	}
 	s, err := model.PromotionSessionGetActive(context.TODO(), p.ID, now)
 	if err != nil {
-		util.GetLoggerEntry(c).Error("get promotion session error", err)
+		util.GetLoggerEntry(ctx).Error("get promotion session error", err)
 		return
 	}
-	_, err = promotion.Claim(c, time.Now(), p, s, user.ID, &user)
+	_, err = promotion.Claim(ctx, time.Now(), p, s, user.ID, &user)
 	if err != nil {
-		util.GetLoggerEntry(c).Error("claim one time bonus error", err)
+		util.GetLoggerEntry(ctx).Error("claim one time bonus error", err)
 		return
 	}
 }
