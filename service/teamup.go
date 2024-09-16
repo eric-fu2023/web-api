863444998ac754a1f9c6fa93a2cc66f83daf24e1
jian <jianhui@redfin.tech>
2024-09-16 11:29:55 +0800

    fixed: fmt.Sprintf does not match the input parameters

--- service/teamup.go
+++ service/teamup.go
@@ -793,10 +793,13 @@ func SendTeamupNotification(teamupType int, userId, percentage, totalTarget, tea
 	notificationTitle := titles[n]
 	notificationMsg := contents[n]
 	if strings.Contains(notificationMsg, "%s") {
-		notificationMsg = fmt.Sprintf(notificationMsg, fmt.Sprintf("%.2f", (float64(percentage)/float64(10000))*(float64(totalTarget)/float64(100))))
+		pString := fmt.Sprintf("%.2f", (float64(percentage)/float64(10000))*(float64(totalTarget)/float64(100)))
+		notificationMsg = fmt.Sprintf(notificationMsg, pString)
 
 		if n != 1 {
-			notificationMsg = fmt.Sprintf(notificationMsg, fmt.Sprintf("%.2f", (float64(percentage)/float64(100)))+"%")
+			pFloat64 := (float64(percentage) / float64(100))
+			notificationMsg = strings.ReplaceAll(notificationMsg, pString, "%s")
+			notificationMsg = fmt.Sprintf(notificationMsg, fmt.Sprintf("%.2f", pFloat64))
 		}
 	}
 
