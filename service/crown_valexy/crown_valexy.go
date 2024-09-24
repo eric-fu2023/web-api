393ca524c741a334ee972bcb859f6fbecf9a294a
noel <noel@redfin.tech>
2024-09-24 18:40:03 +0800

    game: cv remarks only

--- service/crown_valexy/crown_valexy.go
+++ service/crown_valexy/crown_valexy.go
@@ -2,7 +2,6 @@ package crown_valexy
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"strconv"
 	"time"
@@ -53,12 +52,31 @@ func (c *CrownValexy) CreateWallet(ctx context.Context, user model.User, s strin
 
 type Remarks struct {
 	WithdrawId string `json:"withdraw_id"`
-	TransferId string `json:"transfer_id"`
+	DepositId  string `json:"transfer_id"`
 }
 
 func (r Remarks) String() string {
-	bb, _ := json.Marshal(r)
-	return string(bb)
+	if r.WithdrawId != "" {
+		return r.WithdrawId
+	}
+
+	if r.DepositId != "" {
+		return r.DepositId
+	}
+
+	return ""
+}
+
+func (r Remarks) MarshalJSON() ([]byte, error) {
+	if r.WithdrawId != "" {
+		return []byte(r.WithdrawId), nil
+	}
+
+	if r.DepositId != "" {
+		return []byte(r.DepositId), nil
+	}
+
+	return nil, nil
 }
 
 func (c *CrownValexy) TransferFrom(ctx context.Context, tx *gorm.DB, user model.User, _curr string, _gameVendorCode string, gameVendorId int64, extra model.Extra) error {
@@ -76,7 +94,7 @@ func (c *CrownValexy) TransferFrom(ctx context.Context, tx *gorm.DB, user model.
 
 	_, wErr := client.WalletWithdraw(ctx, user.IdAsString(), toWithdraw, Remarks{
 		WithdrawId: withdrawTxId,
-		TransferId: "",
+		DepositId:  "",
 	}.String())
 	if wErr != nil {
 		return wErr
@@ -125,7 +143,7 @@ func (c *CrownValexy) TransferTo(ctx context.Context, tx *gorm.DB, user model.Us
 	depTxId := userId + strconv.FormatInt(time.Now().UnixNano(), 10) + "deposit"
 	_, wErr := client.WalletDeposit(ctx, userId, util.MoneyFloat(sum.Balance), Remarks{
 		WithdrawId: "",
-		TransferId: depTxId,
+		DepositId:  depTxId,
 	}.String())
 	if wErr != nil {
 		return 0, wErr
