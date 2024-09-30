70aafc1a571d3f05b5510dc75c06eb589305e3d0
juicy <juicy@redfin.tech>
2024-09-30 17:45:47 +0800

    [teamup] don't pop spinwheels if dont not set

--- service/teamup.go
+++ service/teamup.go
@@ -896,10 +896,10 @@ func (s TeamupCheckSpinService) CheckSpinPopup(c *gin.Context) (r serializer.Res
 	if shouldPop {
 		teamUpSpinPromotionIdString, _ := model.GetAppConfigWithCache("teamup", "teamup_spin_promotion_id")
 
-		// if teamUpSpinPromotionIdString == "" {
-		// 	spinRes.HasSpin = false
-		// 	return
-		// }
+		if teamUpSpinPromotionIdString == "" || teamUpSpinPromotionIdString == "9999" {
+			spinRes.HasSpin = false
+			return
+		}
 
 		spin, getSpinErr := model.GetSpinByPromotionId(teamUpSpinPromotionIdString)
 		if getSpinErr != nil {
@@ -939,12 +939,12 @@ func (s TeamupCheckSpinService) TeamupSpinResult(c *gin.Context) (r serializer.R
 
 	teamUpSpinPromotionIdString, _ := model.GetAppConfigWithCache("teamup", "teamup_spin_promotion_id")
 
-	// if teamUpSpinPromotionIdString == "" {
-	// 	if err != nil {
-	// 		r = serializer.Err(c, s, serializer.CodeGeneralError, i18n.T("general_error"), err)
-	// 		return
-	// 	}
-	// }
+	if teamUpSpinPromotionIdString == "" || teamUpSpinPromotionIdString == "9999" {
+		if err != nil {
+			r = serializer.Err(c, s, serializer.CodeGeneralError, i18n.T("general_error"), err)
+			return
+		}
+	}
 
 	spin, err := model.GetSpinByPromotionId(teamUpSpinPromotionIdString)
 	if err != nil {
