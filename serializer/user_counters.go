cb8f9405b23a81fa2fddec492e56a29659e9368c
noel <noel@redfin.tech>
2024-10-24 13:20:16 +0800

    formatcounter

--- serializer/user_counters.go
+++ serializer/user_counters.go
@@ -10,22 +10,28 @@ import (
 )
 
 type UserCounters struct {
-	Order              string            `json:"order"`
-	Transaction        string            `json:"transaction"`
-	Notification       string            `json:"notification"`
-	GameOrderHistories map[string]string `json:"game_order_histories"`
+	Order              string             `json:"order"`
+	Transaction        string             `json:"transaction"`
+	Notification       string             `json:"notification"`
+	GameOrderHistories GameOrderHistories `json:"game_order_histories"`
 }
 
-func BuildUserCounters(c *gin.Context, a model.UserCounters, _gameOrderHistories map[game_history_pane.GamesHistoryPaneType]int64) UserCounters {
+type GameOrderHistories struct {
+	ByPaneType map[string]string `json:"by_pane_type"`
+}
+
+func BuildUserCounters(c *gin.Context, a model.UserCounters, _gameOrderHistoriesByPaneType map[game_history_pane.GamesHistoryPaneType]int64) UserCounters {
 	gameOrderHistories := make(map[string]string)
-	for paneType, count := range _gameOrderHistories {
-		gameOrderHistories[strconv.Itoa(int(paneType))] = strconv.Itoa(int(count))
+	for paneType, count := range _gameOrderHistoriesByPaneType {
+		gameOrderHistories[strconv.Itoa(int(paneType))] = formatCounter(count)
 	}
 	return UserCounters{
-		Order:              formatCounter(a.Order),
-		Transaction:        formatCounter(a.Transaction),
-		Notification:       formatCounter(a.Notification),
-		GameOrderHistories: gameOrderHistories,
+		Order:        formatCounter(a.Order),
+		Transaction:  formatCounter(a.Transaction),
+		Notification: formatCounter(a.Notification),
+		GameOrderHistories: GameOrderHistories{
+			ByPaneType: gameOrderHistories,
+		},
 	}
 }
 
