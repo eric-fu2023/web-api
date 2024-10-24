4a21c4c0432378adcee06257cb7e8f58536c0886
noel <noel@redfin.tech>
2024-10-24 15:49:06 +0800

    [badge counter] add gift type

--- service/user_counters.go
+++ service/user_counters.go
@@ -95,13 +95,24 @@ func (service *CounterService) Get(c *gin.Context) serializer.Response {
 	// for others types, count(reports) since last seen.
 	gameHistoryPaneCounts := make(map[game_history_pane.GamesHistoryPaneType]int64)
 	gameHistoryPaneCounts[game_history_pane.GamesPaneAll] = 0
+	var giftcountkey int64 = 3
+
 	for _, gamePane := range game_history_pane.GamePaneHistoryTypes() {
+		pCtx := rfcontext.AppendCallDesc(rfCtx, "counting for game history type: "+strconv.Itoa(int(gamePane)))
+
 		if gamePane == game_history_pane.GamesPaneAll {
 			continue
 		}
+
+		if gamePane == giftcountkey {
+			err := fmt.Errorf("conflict: game pane type=3 is reserved for giftcountkey")
+			pCtx = rfcontext.AppendError(pCtx, err, "counting for game history pane")
+			log.Println(rfcontext.Fmt(pCtx))
+			return serializer.Err(c, service, 500, i18n.T("general_error"), err)
+		}
+
 		gameHistoryPaneCounts[gamePane] = 0
 
-		pCtx := rfcontext.AppendCallDesc(rfCtx, "counting for game history type: "+strconv.Itoa(int(gamePane)))
 		lastSeen, err := counter.LastSeenForGamePane(gamePane)
 		if err != nil {
 			pCtx = rfcontext.AppendErrorAsWarn(pCtx, fmt.Errorf("%v %v", err, gamePane), "getting column name for game pane")
@@ -121,11 +132,14 @@ func (service *CounterService) Get(c *gin.Context) serializer.Response {
 	}
 
 	var giftUnseenCount int64 = 0
-	data, err := serializer.BuildUserCounters(counters, gameHistoryPaneCounts, giftUnseenCount)
+	gameHistoryPaneCounts[giftcountkey] = giftUnseenCount
+	gameHistoryPaneCounts[game_history_pane.GamesPaneAll] += giftUnseenCount
+
+	data, err := serializer.BuildUserCounters(counters, gameHistoryPaneCounts)
 	if err != nil {
 		rfCtx = rfcontext.AppendError(rfCtx, err, "BuildUserCounters")
 		log.Println(rfcontext.Fmt(rfCtx))
-		return serializer.DBErr(c, service, i18n.T("general_error"), err)
+		return serializer.Err(c, service, 500, i18n.T("general_error"), err)
 	}
 	responseBody := serializer.Response{
 		Data: data,
