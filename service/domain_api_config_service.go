d07a8741f511e3a79ec0ed45d6c9f1a2be4f13a3
juicy <juicy@redfin.tech>
2024-11-11 10:58:06 +0800

    Added t_m
    (cherry picked from commit 7245f7ceab862e13db766f04cd3ca4f4d7cc8cf4)

--- service/domain_api_config_service.go
+++ service/domain_api_config_service.go
@@ -1,8 +1,14 @@
 package service
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"time"
 	"web-api/cache"
 	"web-api/model"
@@ -31,6 +37,22 @@ type DomainConfigRes struct {
 	CrickongApi string `json:"c,omitempty"`
 	// for ba only, A screen or B
 	Mode bool `json:"m"`
+	// mode from Taiwan Team
+	TaiwanMode TaiwanModeResponse `json:"t_m"`
+}
+
+type CallbackSecretData struct {
+	SecretData string `json:"secretData"`
+}
+
+type TaiwanModeResponse struct {
+	IsValid   bool     `json:"isValid"`
+	UpdateUrl string   `json:"updateUrl"`
+	BaseUrls  []string `json:"BaseUrls"`
+	WssUrl    string   `json:"WssUrl"`
+	ImgUrl    string   `json:"ImgUrl"`
+	Ip        string   `json:"Ip"`
+	Location  string   `json:"Location"`
 }
 
 func (service *DomainConfigService) InitApp(c *gin.Context) (code int, res serializer.Response, err error) {
@@ -44,7 +66,40 @@ func (service *DomainConfigService) InitApp(c *gin.Context) (code int, res seria
 		return
 	}
 
-	// validABScreenHeaders := checkABScreenHeaders(c)
+	queryParameters := generateParametersByHeaderIsAB(c)
+
+	var twResp TaiwanModeResponse
+	if len(queryParameters) != 0 {
+		jsonData, _ := json.Marshal(queryParameters)
+
+		pureSgInitAppUrl := os.Getenv("PURE_SG_DOMAIN") + "/ajax/public/ios/initial-app"
+		fmt.Println(pureSgInitAppUrl)
+		req, _ := http.NewRequest("POST", pureSgInitAppUrl, bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Println("Error creating request:", err)
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Secret-Data", "true")
+
+		client := &http.Client{}
+		resp, respErr := client.Do(req)
+		if respErr != nil {
+			fmt.Println("Error sending request:", err)
+		}
+		defer resp.Body.Close()
+
+		body, _ := io.ReadAll(resp.Body)
+
+		fmt.Println("body")
+		fmt.Println(string(body))
+		var respData CallbackSecretData
+		_ = json.Unmarshal(body, &respData)
+		respJson, _ := util.DecryptPureSGSecret(respData.SecretData)
+
+		_ = json.Unmarshal([]byte(respJson), &twResp)
+
+	}
 
 	// TEMPORARY - get ab screen status from app config
 	// Pending API from Taiwan
@@ -66,7 +121,8 @@ func (service *DomainConfigService) InitApp(c *gin.Context) (code int, res seria
 			CrickongApi: FindRandomDomain(model.SupportTypeApp, model.DomainTypeCrickong, c),
 			// Mode = true = Is A Screen
 			// Mode = false = Is Not A Screen
-			Mode: isA,
+			Mode:       isA,
+			TaiwanMode: twResp,
 		},
 	}
 
@@ -136,30 +192,60 @@ func retrieveFromDB(supportType string, domainType string, c *gin.Context) strin
 	return ""
 }
 
-func checkABScreenHeaders(c *gin.Context) (isValid bool) {
+func generateParametersByHeaderIsAB(c *gin.Context) (res map[string]interface{}) {
 
 	// Headers
 	// 设备号 - DeviceUuid
 	// 是否为Ipad - IsIpad
 	// VPN状态 - IsVpn
 	// 充电状态 - IsCharging
-	// 产品代号 -
-	// 母包版本 -
-	// 上架包 ｜ 非上架包 - IsOnStore
+	// 产品代号 - ProductCode
+	// 版本 - Version
 
 	// Headers to decide A/B screen
 	// c.GetHeader("DeviceUuid")
 	// c.GetHeader("IsIpad")
 	// c.GetHeader("IsVpn")
 	// c.GetHeader("IsCharging")
-	// c.GetHeader("IsOnStore")
-	// c.GetHeader("产品代号")
-	// c.GetHeader("母包版本")
+	// c.GetHeader("ProductCode")
+	// c.GetHeader("Version")
 
-	if c.GetHeader("DeviceUuid") == "" || c.GetHeader("IsIpad") == "" || c.GetHeader("IsVpn") == "" || c.GetHeader("IsCharging") == "" || c.GetHeader("IsOnStore") == "" || c.GetHeader("产品代号") == "" || c.GetHeader("母包版本") == "" {
+	if c.GetHeader("DeviceUuid") == "" || c.GetHeader("IsIpad") == "" || c.GetHeader("IsVpn") == "" || c.GetHeader("IsCharging") == "" || c.GetHeader("ProductCode") == "" || c.GetHeader("Version") == "" {
 		return
 	}
 
-	isValid = true
+	queryParameters := map[string]interface{}{
+		"deviceNumber": c.GetHeader("DeviceUuid"),
+		"isCharging":   false,
+		"isVpn":        false,
+		"isIpad":       false,
+		"productCode":  c.GetHeader("ProductCode"),
+		"version":      c.GetHeader("Version"),
+	}
+
+	if c.GetHeader("IsCharging") == "true" {
+		queryParameters["isCharging"] = true
+	}
+
+	if c.GetHeader("IsVpn") == "true" {
+		queryParameters["isVpn"] = true
+	}
+
+	if c.GetHeader("IsIpad") == "true" {
+		queryParameters["isIpad"] = true
+	}
+
+	jsonData, err := json.Marshal(queryParameters)
+	if err != nil {
+		log.Fatalf("Error encoding JSON: %v", err)
+	}
+
+	// Print the JSON result as a string
+	fmt.Println(string(jsonData))
+	abRes, _ := util.EncryptPureSGSecret(string(jsonData))
+	queryParameters["SecretData"] = abRes
+
+	res = queryParameters
+
 	return
 }
