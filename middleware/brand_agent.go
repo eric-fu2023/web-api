9f815a409b2a9cdeeee9b3656b3c5e4c8b9a3965
Du Wei <perfuma@redfin.tech>
2024-11-11 16:20:55 +0800

    [pixel_app] add set("_agent")

--- middleware/brand_agent.go
+++ middleware/brand_agent.go
@@ -1,9 +1,10 @@
 package middleware
 
 import (
-	"github.com/gin-gonic/gin"
 	"strconv"
 	"web-api/serializer"
+
+	"github.com/gin-gonic/gin"
 )
 
 func BrandAgent() gin.HandlerFunc {
@@ -28,18 +29,23 @@ func BrandAgent() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
-		//agent, err := strconv.Atoi(c.GetHeader("Agent"))
-		//if err != nil {
-		//	c.JSON(400, serializer.Response{
-		//		Code:  serializer.CodeParamErr,
-		//		Msg:   "Invalid headers",
-		//		Error: err.Error(),
-		//	})
-		//	c.Abort()
-		//	return
-		//}
+		
+		// should use agent id, but now we need to send data to pixel......
+		agent, err := strconv.Atoi(c.GetHeader("Agent"))
+		if err != nil {
+			c.JSON(400, serializer.Response{
+				Code:  serializer.CodeParamErr,
+				Msg:   "Invalid headers",
+				Error: err.Error(),
+			})
+			c.Abort()
+			return
+		}
+		c.Set("_agent", agent)
+
+
+
 		c.Set("_brand", brand)
-		//c.Set("_agent", agent)
 		c.Next()
 	}
 }
