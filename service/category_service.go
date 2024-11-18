1c4e70494ee6e548941d550297523eb86f619028
Du Wei <perfuma@redfin.tech>
2024-11-18 15:24:56 +0800

    Revert "[ios app] A side need to show cricket category only"
    This reverts commit 941136dcbef927da9918db4ba29d870da5ec61f3.

--- service/category_service.go
+++ service/category_service.go
@@ -1,12 +1,11 @@
 package service
 
 import (
+	ploutos "blgit.rfdev.tech/taya/ploutos-object"
+	"github.com/gin-gonic/gin"
 	"web-api/model"
 	"web-api/serializer"
 	"web-api/util/i18n"
-
-	ploutos "blgit.rfdev.tech/taya/ploutos-object"
-	"github.com/gin-gonic/gin"
 )
 
 type CategoryListService struct {
@@ -22,40 +21,6 @@ func (service *CategoryListService) List(c *gin.Context) (r serializer.Response,
 	var list []serializer.CategoryType
 	for _, category := range categories {
 		list = append(list, serializer.BuildCategoryType(category))
-	}
-
-	isA := false
-	if v, exists := c.Get("_isA"); exists {
-		if vv, ok := v.(bool); ok && vv {
-			isA = vv
-		}
-	}
-	if isA {
-		filteredList := []serializer.CategoryType{} // Assuming list is of type []serializer.ListItem
-
-		for _, listItem := range list {
-			if listItem.ID == 3 { // Condition for "batace"
-				filteredCategories := []serializer.Category{}
-
-				for _, category := range listItem.Categories {
-					if category.ID == 4 { // Condition for "cricket"
-						filteredCategories = append(filteredCategories, category)
-						break // Stop after finding the matching category
-					}
-				}
-
-				// If the listItem has the required category, update and keep it in the list
-				if len(filteredCategories) > 0 {
-					listItem.Categories = filteredCategories
-					filteredList = append(filteredList, listItem)
-				}
-
-			}
-
-		}
-
-		// Replace the original list with the filtered version
-		list = filteredList
 	}
 
 	r = serializer.Response{
